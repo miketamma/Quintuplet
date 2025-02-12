9020ea675a0b4a61b8e30eade9477ab7d520474c
Michele <mike.tamma@gmail.com>
2025-02-12 16:41:12 +0100

    typo check

--- Boltzmann_Solver.py
+++ Boltzmann_Solver.py
@@ -20,7 +20,7 @@ mp.dps = 25; mp.pretty = True
 n_scan = int(input("Input number of masses to scan: "))
 
 
-M_DM_scan = np.linspace(3 * TeV, 14 * TeV, n_scan)
+M_DM_scan = np.linspace(3 * TeV, 15 * TeV, n_scan)
 
 
 print("""
@@ -39,6 +39,8 @@ print("""
 
     6 - Network of Boltzmann equations with 1s bound states (~8.6 s/mass)
 
+    7 - Network of Boltzmann equations with 1s, 2s, 2p bound states (~30 s/mass)
+
     0 - Solve all of them (could take some time)
     
 
@@ -47,7 +49,7 @@ print("""
 
 which_case = int(input("Input case: "))
 
-possible_cases = [0, 1, 2, 3, 4, 5, 6]
+possible_cases = [0, 1, 2, 3, 4, 5, 6, 7]
 
 if which_case not in possible_cases: 
     print("WRONG")
@@ -59,15 +61,6 @@ if len(print_results) == 0: print_results = "n"
 
 
 
-
-############################################################################
-
-start = time.time()
-
-############################################################################
-
-
-
 ############################################################################
 
 # Numerical ODE solver, n is the number of variables in the ODE
@@ -75,7 +68,7 @@ start = time.time()
 ############################################################################
 
 def NDE_solver(Boltz, n):
-    y0 = np.full( n, [1.e-5,])
+    y0 = np.concatenate( ([5e-3], np.full( n, 1.e-30) ) ) #np.full( n, 1.e-30)
     teval = np.logspace(np.log10(3.5), 5.9, 100)
     tspan=[3.0, 1.e6]
     
@@ -107,7 +100,7 @@ def do_scan_Boltzmann_Tree():
 
         bb = lambda z, Y: Boltzmann_Tree(DM, z, Y)
 
-        solution = NDE_solver(bb, 1)
+        solution = NDE_solver(bb, 0)
 
         omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
 
@@ -146,7 +139,7 @@ def do_scan_Boltzmann_Hulthen_free():
 
         bb = lambda z, Y: Boltzmann_Hulthen_free(DM, z, Y)
 
-        solution = NDE_solver(bb, 1)
+        solution = NDE_solver(bb, 0)
 
         omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
 
@@ -214,7 +207,7 @@ def do_scan_Boltzmann_Hulthen_1s_effective():
 
         bb = lambda z, Y: Boltzmann_Hulthen_effective(DM, BS_1s_list, z, Y)
 
-        solution = NDE_solver(bb, 1)
+        solution = NDE_solver(bb, 0)
         
         omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
 
@@ -233,16 +226,6 @@ def do_scan_Boltzmann_Hulthen_1s_effective():
 # Define Boltzmann equation with 1s, 2s, 2p BS (effective equation)
 
 ############################################################################ 
-
-
-# BS_1s1 = Quintuplet_BS(10 * TeV, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
-# BS_1s3 = Quintuplet_BS(10 * TeV, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
-# BS_1s5 = Quintuplet_BS(10 * TeV, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
-
-# BS_1s_list = [BS_1s1,BS_1s3,BS_1s5] 
-
-# print( BS_eff(BS_1s1, 10 * TeV, 1e3) )
-# print( np.sum([BS_eff(BS, 10 * TeV, 1e3) for BS in BS_1s_list] ) )
 
 
 
@@ -272,7 +255,7 @@ def do_scan_Boltzmann_Hulthen_1s2s2p_effective():
 
         bb = lambda z, Y: Boltzmann_Hulthen_effective(DM, BS_list, z, Y)
 
-        solution = NDE_solver(bb, 1)
+        solution = NDE_solver(bb, 0)
         
         omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
 
@@ -301,6 +284,16 @@ def Yratio(BS, M, z):
     factor_z = ( np.exp( z * BS.binding_energy_BS(z) ) )/( z**(3/2) )
 
     return prefactor * prefactor_g * factor_z
+
+# def Yratio(BS, M, z):
+
+#     prefactor = ( pi**(5/2) ) * 32/45
+
+#     prefactor_g = BS.gI * gs_star(M/z)/( gx**2 )
+
+#     factor_z = ( np.exp( - z * BS.binding_energy_BS(z) ) ) * ( z**(3/2) )
+
+#     return factor_z/( prefactor * prefactor_g )
 
 def Yratio_NoExp(BS, M, z):
 
@@ -369,7 +362,7 @@ def do_scan_Boltzmann_Hulthen_Network_1s1():
 
         bb = lambda z, Y: Boltzmann_Hulthen_Network_1s1(DM, BS_1s1, z, Y)
 
-        solution = NDE_solver(bb, 2)
+        solution = NDE_solver(bb, 1)
         
         omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
 
@@ -405,7 +398,6 @@ def Boltzmann_Hulthen_Network_1s(DM, BS_list, z, Y):
     ### Boltzmann equation for DM ###
 
     prefactor_DM = - DM.entropy(z)/( z * DM.Hubble(z) )
-    prefactor_I = 1/( z * DM.Hubble(z) )
 
     dYdz_DM_1 = ( Y_DM**2 - DM.Yeq(z)**2 ) * DM.sv_production_Somm_Hulthen(z)
     dYdz_DM_2 = sigma0_prime(M) * np.sum( [ BS_list[i].bsf(z) * ( Y_DM**2 - Y_1s[i]/Yratio(BS_list[i], M, z) ) for i in range(len(BS_list)) ] )
@@ -416,23 +408,25 @@ def Boltzmann_Hulthen_Network_1s(DM, BS_list, z, Y):
 
     prefactor_I = 1/( z * DM.Hubble(z) )
 
-    dYdz_1s_1 = [ BS.Gamma_break_NoExp(z) * Yratio_NoExp(BS, M, z) * Y_DM**2 for BS in BS_list ]
-    dYdz_1s_2 = [- BS_list[i].Gamma_break(z) * Y_1s[i] for i in range(len(BS_list)) ]
-    dYdz_1s_3 = [ BS_list[i].Gamma_ann_Hulthen_TA(z) * ( BS_list[i].Y_BS_eq(z) - Y_1s[i] ) for i in range(len(BS_list)) ]
+    dYdz_1s = []
 
-    dYdz_1s = prefactor_I * np.array( [ ( dYdz_1s_1[i] + dYdz_1s_2[i] + dYdz_1s_3[i] ) for i in range(len(BS_list)) ] )
+    for i in range(len(BS_list)):
+        dYdz_1s_1 = BS_list[i].Gamma_break_NoExp(z) * Yratio_NoExp(BS_list[i], M, z) * Y_DM**2 
+        dYdz_1s_2 = BS_list[i].Gamma_break(z) * Y_1s[i]  
+        dYdz_1s_3 = BS_list[i].Gamma_ann_Hulthen_TA(z) * ( BS_list[i].Y_BS_eq(z) - Y_1s[i] )
+
+        dYdz_1s.append( prefactor_I * ( dYdz_1s_1 - dYdz_1s_2 + dYdz_1s_3 ) )
+
+    # dYdz_1s_1 = [ BS_list[i].Gamma_break_NoExp(z) * Yratio_NoExp(BS_list[i], M, z) * Y_DM**2 for i in range(len(BS_list)) ]
+    # dYdz_1s_2 = [ BS_list[i].Gamma_break(z) * Y_1s[i] for i in range(len(BS_list)) ]
+    # dYdz_1s_3 = [ BS_list[i].Gamma_ann_Hulthen_TA(z) * ( BS_list[i].Y_BS_eq(z) - Y_1s[i] ) for i in range(len(BS_list)) ]
+
+    # dYdz_1s = prefactor_I * np.array( [ ( dYdz_1s_1[i] - dYdz_1s_2[i] + dYdz_1s_3[i] ) for i in range(len(BS_list)) ] )
 
     dYdz = np.concatenate( ([dYdz_DM], dYdz_1s) )
 
     return dYdz
 
-# DM = Quintuplet_DM(10*TeV)
-# BS_1s1 = Quintuplet_BS(10*TeV, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
-# BS_1s3 = Quintuplet_BS(10*TeV, gI_list[1], nE_list[1], l_list[1], Isp_list[1], nS_Quint, BS_func_list[1], gf_list[1])
-# BS_1s5 = Quintuplet_BS(10*TeV, gI_list[2], nE_list[2], l_list[2], Isp_list[2], nS_Quint, BS_func_list[2], gf_list[2])
-# BS_list = [BS_1s1, BS_1s3, BS_1s5]
-
-# print( Boltzmann_Hulthen_Network_1s(DM, BS_list, 100, [0,0,0,0]) )
 
 def do_scan_Boltzmann_Hulthen_Network_1s():
 
@@ -444,14 +438,14 @@ def do_scan_Boltzmann_Hulthen_Network_1s():
 
         # # Variable order is: (Mass, gI, nE, l, I, nS, BS function, group factor constant)
 
-        BS_1s1 = Quintuplet_BS(10*TeV, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
-        BS_1s3 = Quintuplet_BS(10*TeV, gI_list[1], nE_list[1], l_list[1], Isp_list[1], nS_Quint, BS_func_list[1], gf_list[1])
-        BS_1s5 = Quintuplet_BS(10*TeV, gI_list[2], nE_list[2], l_list[2], Isp_list[2], nS_Quint, BS_func_list[2], gf_list[2])
+        BS_1s1 = Quintuplet_BS(M_DM, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
+        BS_1s3 = Quintuplet_BS(M_DM, gI_list[1], nE_list[1], l_list[1], Isp_list[1], nS_Quint, BS_func_list[1], gf_list[1])
+        BS_1s5 = Quintuplet_BS(M_DM, gI_list[2], nE_list[2], l_list[2], Isp_list[2], nS_Quint, BS_func_list[2], gf_list[2])
         BS_list = [BS_1s1, BS_1s3, BS_1s5]
 
         bb = lambda z, Y: Boltzmann_Hulthen_Network_1s(DM, BS_list, z, Y)
 
-        solution = NDE_solver(bb, 4)
+        solution = NDE_solver(bb, len(BS_list) )
         
         omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
 
@@ -464,11 +458,112 @@ def do_scan_Boltzmann_Hulthen_Network_1s():
     np.savetxt('./Results/Omega_Boltzmann_Hulthen_Network_1s.txt', Omega_Boltzmann_Hulthen_Network_1s)
 
 
+
+############################################################################
+
+# Define Boltzmann equation Network with all 1s, 2p, 2s BS
+
+############################################################################ 
+
+
+def Boltzmann_Hulthen_Network_1s2s2p(DM, BS_list, z, Y):
+
+
+    Y_DM = Y[0]
+    Y_1s1 = Y[1]
+    Y_1s3 = Y[2]
+    Y_1s5 = Y[3]
+    Y_2s1 = Y[4]
+    Y_2s3 = Y[5]
+    Y_2s5 = Y[6]
+    Y_2p1 = Y[7]
+    Y_2p3 = Y[8]
+    Y_2p5 = Y[9]
+    Y_BS = [Y_1s1, Y_1s3, Y_1s5, Y_2s1, Y_2s3, Y_2s5, Y_2p1, Y_2p3, Y_2p5]
+
+    ### Define DM mass ###
+
+    M = DM.M
+
+    ### Boltzmann equation for DM ###
+
+    prefactor_DM = - DM.entropy(z)/( z * DM.Hubble(z) )
+
+    dYdz_DM_1 = ( Y_DM**2 - DM.Yeq(z)**2 ) * DM.sv_production_Somm_Hulthen(z)
+    dYdz_DM_2 = sigma0_prime(M) * np.sum( [ BS_list[i].bsf(z) * ( Y_DM**2 - Y_BS[i]/Yratio(BS_list[i], M, z) ) for i in range(len(BS_list)) ] )
+
+    dYdz_DM = prefactor_DM * ( dYdz_DM_1 + dYdz_DM_2 )
+
+    ### Boltzmann equation for BS ###
+
+    prefactor_I = 1/( z * DM.Hubble(z) )
+
+    dYdz_BS_1 = [ BS_list[i].Gamma_break_NoExp(z) * Yratio_NoExp(BS_list[i], M, z) * Y_DM**2 for i in range(len(BS_list)) ]
+    dYdz_BS_2 = [ BS_list[i].Gamma_break(z) * Y_BS[i] for i in range(len(BS_list)) ]
+    dYdz_BS_3 = [ BS_list[i].Gamma_ann_Hulthen_TA(z) * ( BS_list[i].Y_BS_eq(z) - Y_BS[i] ) for i in range(len(BS_list)) ]
+
+    dYdz_BS = prefactor_I * np.array( [ ( dYdz_BS_1[i] - dYdz_BS_2[i] + dYdz_BS_3[i] ) for i in range(len(BS_list)) ] )
+
+    dYdz = np.concatenate( ([dYdz_DM], dYdz_BS) )
+
+    return dYdz
+
+
+
+def do_scan_Boltzmann_Hulthen_Network_1s2s2p():
+
+    Omega_Boltzmann_Hulthen_Network_1s2s2p = []
+
+    for M_DM in M_DM_scan:
+        
+        DM = Quintuplet_DM(M_DM)
+
+        # # Variable order is: (Mass, gI, nE, l, I, nS, BS function, group factor constant)
+
+        BS_1s1 = Quintuplet_BS(M_DM, gI_list[0], nE_list[0], l_list[0], Isp_list[0], nS_Quint, BS_func_list[0], gf_list[0])
+        BS_1s3 = Quintuplet_BS(M_DM, gI_list[1], nE_list[1], l_list[1], Isp_list[1], nS_Quint, BS_func_list[1], gf_list[1])
+        BS_1s5 = Quintuplet_BS(M_DM, gI_list[2], nE_list[2], l_list[2], Isp_list[2], nS_Quint, BS_func_list[2], gf_list[2])
+
+        BS_2s1 = Quintuplet_BS(M_DM, gI_list[3], nE_list[3], l_list[3], Isp_list[3], nS_Quint, BS_func_list[3], gf_list[3])
+        BS_2s3 = Quintuplet_BS(M_DM, gI_list[4], nE_list[4], l_list[4], Isp_list[4], nS_Quint, BS_func_list[4], gf_list[4])
+        BS_2s5 = Quintuplet_BS(M_DM, gI_list[5], nE_list[5], l_list[5], Isp_list[5], nS_Quint, BS_func_list[5], gf_list[5])
+
+        BS_2p1 = Quintuplet_BS(M_DM, gI_list[6], nE_list[6], l_list[6], Isp_list[6], nS_Quint, BS_func_list[6], gf_list[6])
+        BS_2p3 = Quintuplet_BS(M_DM, gI_list[7], nE_list[7], l_list[7], Isp_list[7], nS_Quint, BS_func_list[7], gf_list[7])
+        BS_2p5 = Quintuplet_BS(M_DM, gI_list[8], nE_list[8], l_list[8], Isp_list[8], nS_Quint, BS_func_list[8], gf_list[8])
+
+        BS_list = [BS_1s1, BS_1s3, BS_1s5, BS_2s1, BS_2s3, BS_2s5, BS_2p1, BS_2p3, BS_2p5]
+
+        bb = lambda z, Y: Boltzmann_Hulthen_Network_1s2s2p(DM, BS_list, z, Y)
+
+        solution = NDE_solver(bb, len(BS_list) )
+        
+        try:
+            omega_solution = Omega_DM(1.0, solution.y[0][-1], M_DM)
+            Omega_Boltzmann_Hulthen_Network_1s2s2p.append([M_DM, omega_solution])
+
+            if print_results == "y":
+                print( [ M_DM, omega_solution ] )
+            else: continue
+        
+        except IndexError:
+            pass
+
+    np.savetxt('./Results/Omega_Boltzmann_Hulthen_Network_1s2s2p.txt', Omega_Boltzmann_Hulthen_Network_1s2s2p)
+
+
 ############################################################################
 
 # Do selected scan
 
 ############################################################################ 
+
+############################################################################
+
+start = time.time()
+
+############################################################################
+
 
 if which_case == 0:
     # Scan with free Boltzmann equation
@@ -483,6 +578,8 @@ if which_case == 0:
     do_scan_Boltzmann_Hulthen_Network_1s1()
     # Scan with network of Boltzmann equations, using 1s BS
     do_scan_Boltzmann_Hulthen_Network_1s()
+    # Scan with network of Boltzmann equations, using all 1s, 2s, 2p BS
+    do_scan_Boltzmann_Hulthen_Network_1s2s2p()
 elif which_case == 1:
     do_scan_Boltzmann_Tree()
 elif which_case == 2:
@@ -495,8 +592,13 @@ elif which_case == 5:
     do_scan_Boltzmann_Hulthen_Network_1s1()
 elif which_case == 6:
     do_scan_Boltzmann_Hulthen_Network_1s()
+elif which_case == 7:
+    do_scan_Boltzmann_Hulthen_Network_1s2s2p()
 
 
+############################################################################
 
 end = time.time()
 print("Total computing time: ", end - start)
+
+############################################################################
